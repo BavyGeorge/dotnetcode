57a6406c5264c77748235b8526bcac555699c1dc
Bavy Tess <bavythom@gmail.com>
2020-09-16 14:55:09 +1200

    First commit

---
+++ RacingGameTestProject/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace RacingGameTestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RoadDivider1 = new System.Windows.Forms.PictureBox();
+            this.RoadAnimationTimer = new System.Windows.Forms.Timer(this.components);
+            this.RoadDivider2 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider3 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider4 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider5 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider6 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider7 = new System.Windows.Forms.PictureBox();
+            this.RoadDivider8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.Car = new System.Windows.Forms.PictureBox();
+            this.Left_mover = new System.Windows.Forms.Timer(this.components);
+            this.Right_mover = new System.Windows.Forms.Timer(this.components);
+            this.EnemyCar1 = new System.Windows.Forms.PictureBox();
+            this.EnemyCar2 = new System.Windows.Forms.PictureBox();
+            this.EnemyCar3 = new System.Windows.Forms.PictureBox();
+            this.Enemy1_timer = new System.Windows.Forms.Timer(this.components);
+            this.Enemy2_timer = new System.Windows.Forms.Timer(this.components);
+            this.Enemy3_timer = new System.Windows.Forms.Timer(this.components);
+            this.GameOverLabel = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.ReplayButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RoadDivider1
+            // 
+            this.RoadDivider1.BackColor = System.Drawing.Color.White;
+            this.RoadDivider1.Location = new System.Drawing.Point(108, -3);
+            this.RoadDivider1.Name = "RoadDivider1";
+            this.RoadDivider1.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider1.TabIndex = 0;
+            this.RoadDivider1.TabStop = false;
+            // 
+            // RoadAnimationTimer
+            // 
+            this.RoadAnimationTimer.Enabled = true;
+            this.RoadAnimationTimer.Interval = 10;
+            this.RoadAnimationTimer.Tick += new System.EventHandler(this.RoadAnimationTimer_Tick);
+            // 
+            // RoadDivider2
+            // 
+            this.RoadDivider2.BackColor = System.Drawing.Color.White;
+            this.RoadDivider2.Location = new System.Drawing.Point(265, -3);
+            this.RoadDivider2.Name = "RoadDivider2";
+            this.RoadDivider2.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider2.TabIndex = 0;
+            this.RoadDivider2.TabStop = false;
+            // 
+            // RoadDivider3
+            // 
+            this.RoadDivider3.BackColor = System.Drawing.Color.White;
+            this.RoadDivider3.Location = new System.Drawing.Point(108, 86);
+            this.RoadDivider3.Name = "RoadDivider3";
+            this.RoadDivider3.Size = new System.Drawing.Size(10, 56);
+            this.RoadDivider3.TabIndex = 0;
+            this.RoadDivider3.TabStop = false;
+            // 
+            // RoadDivider4
+            // 
+            this.RoadDivider4.BackColor = System.Drawing.Color.White;
+            this.RoadDivider4.Location = new System.Drawing.Point(265, 86);
+            this.RoadDivider4.Name = "RoadDivider4";
+            this.RoadDivider4.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider4.TabIndex = 0;
+            this.RoadDivider4.TabStop = false;
+            // 
+            // RoadDivider5
+            // 
+            this.RoadDivider5.BackColor = System.Drawing.Color.White;
+            this.RoadDivider5.Location = new System.Drawing.Point(108, 178);
+            this.RoadDivider5.Name = "RoadDivider5";
+            this.RoadDivider5.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider5.TabIndex = 0;
+            this.RoadDivider5.TabStop = false;
+            // 
+            // RoadDivider6
+            // 
+            this.RoadDivider6.BackColor = System.Drawing.Color.White;
+            this.RoadDivider6.Location = new System.Drawing.Point(265, 178);
+            this.RoadDivider6.Name = "RoadDivider6";
+            this.RoadDivider6.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider6.TabIndex = 0;
+            this.RoadDivider6.TabStop = false;
+            // 
+            // RoadDivider7
+            // 
+            this.RoadDivider7.BackColor = System.Drawing.Color.White;
+            this.RoadDivider7.Location = new System.Drawing.Point(108, 270);
+            this.RoadDivider7.Name = "RoadDivider7";
+            this.RoadDivider7.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider7.TabIndex = 0;
+            this.RoadDivider7.TabStop = false;
+            // 
+            // RoadDivider8
+            // 
+            this.RoadDivider8.BackColor = System.Drawing.Color.White;
+            this.RoadDivider8.Location = new System.Drawing.Point(265, 270);
+            this.RoadDivider8.Name = "RoadDivider8";
+            this.RoadDivider8.Size = new System.Drawing.Size(10, 71);
+            this.RoadDivider8.TabIndex = 0;
+            this.RoadDivider8.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(282, 243);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(8, 8);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Car
+            // 
+            this.Car.Image = ((System.Drawing.Image)(resources.GetObject("Car.Image")));
+            this.Car.Location = new System.Drawing.Point(165, 214);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(51, 90);
+            this.Car.TabIndex = 2;
+            this.Car.TabStop = false;
+            // 
+            // Left_mover
+            // 
+            this.Left_mover.Interval = 10;
+            this.Left_mover.Tick += new System.EventHandler(this.Left_mover_Tick);
+            // 
+            // Right_mover
+            // 
+            this.Right_mover.Interval = 10;
+            this.Right_mover.Tick += new System.EventHandler(this.Right_mover_Tick);
+            // 
+            // EnemyCar1
+            // 
+            this.EnemyCar1.Image = ((System.Drawing.Image)(resources.GetObject("EnemyCar1.Image")));
+            this.EnemyCar1.Location = new System.Drawing.Point(12, 206);
+            this.EnemyCar1.Name = "EnemyCar1";
+            this.EnemyCar1.Size = new System.Drawing.Size(54, 98);
+            this.EnemyCar1.TabIndex = 3;
+            this.EnemyCar1.TabStop = false;
+            // 
+            // EnemyCar2
+            // 
+            this.EnemyCar2.Image = ((System.Drawing.Image)(resources.GetObject("EnemyCar2.Image")));
+            this.EnemyCar2.Location = new System.Drawing.Point(165, 12);
+            this.EnemyCar2.Name = "EnemyCar2";
+            this.EnemyCar2.Size = new System.Drawing.Size(55, 92);
+            this.EnemyCar2.TabIndex = 4;
+            this.EnemyCar2.TabStop = false;
+            // 
+            // EnemyCar3
+            // 
+            this.EnemyCar3.Image = ((System.Drawing.Image)(resources.GetObject("EnemyCar3.Image")));
+            this.EnemyCar3.Location = new System.Drawing.Point(320, 12);
+            this.EnemyCar3.Name = "EnemyCar3";
+            this.EnemyCar3.Size = new System.Drawing.Size(50, 94);
+            this.EnemyCar3.TabIndex = 5;
+            this.EnemyCar3.TabStop = false;
+            // 
+            // Enemy1_timer
+            // 
+            this.Enemy1_timer.Enabled = true;
+            this.Enemy1_timer.Interval = 10;
+            this.Enemy1_timer.Tick += new System.EventHandler(this.Enemy1_timer_Tick);
+            // 
+            // Enemy2_timer
+            // 
+            this.Enemy2_timer.Enabled = true;
+            this.Enemy2_timer.Interval = 10;
+            this.Enemy2_timer.Tick += new System.EventHandler(this.Enemy2_timer_Tick);
+            // 
+            // Enemy3_timer
+            // 
+            this.Enemy3_timer.Enabled = true;
+            this.Enemy3_timer.Interval = 10;
+            this.Enemy3_timer.Tick += new System.EventHandler(this.Enemy3_timer_Tick);
+            // 
+            // GameOverLabel
+            // 
+            this.GameOverLabel.AutoSize = true;
+            this.GameOverLabel.BackColor = System.Drawing.Color.White;
+            this.GameOverLabel.Font = new System.Drawing.Font("Sitka Display", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.GameOverLabel.ForeColor = System.Drawing.Color.Red;
+            this.GameOverLabel.Location = new System.Drawing.Point(101, 109);
+            this.GameOverLabel.Name = "GameOverLabel";
+            this.GameOverLabel.Size = new System.Drawing.Size(174, 48);
+            this.GameOverLabel.TabIndex = 6;
+            this.GameOverLabel.Text = "Game Over";
+            this.GameOverLabel.Visible = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.ScoreLabel.Location = new System.Drawing.Point(34, 25);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(69, 23);
+            this.ScoreLabel.TabIndex = 7;
+            this.ScoreLabel.Text = "Score 0";
+            // 
+            // ReplayButton
+            // 
+            this.ReplayButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ReplayButton.ForeColor = System.Drawing.Color.Green;
+            this.ReplayButton.Location = new System.Drawing.Point(141, 179);
+            this.ReplayButton.Name = "ReplayButton";
+            this.ReplayButton.Size = new System.Drawing.Size(90, 51);
+            this.ReplayButton.TabIndex = 8;
+            this.ReplayButton.Text = "Replay";
+            this.ReplayButton.UseVisualStyleBackColor = true;
+            this.ReplayButton.Visible = false;
+            this.ReplayButton.Click += new System.EventHandler(this.ReplayButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.ReplayButton);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.GameOverLabel);
+            this.Controls.Add(this.EnemyCar3);
+            this.Controls.Add(this.EnemyCar2);
+            this.Controls.Add(this.EnemyCar1);
+            this.Controls.Add(this.Car);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.RoadDivider8);
+            this.Controls.Add(this.RoadDivider7);
+            this.Controls.Add(this.RoadDivider6);
+            this.Controls.Add(this.RoadDivider5);
+            this.Controls.Add(this.RoadDivider4);
+            this.Controls.Add(this.RoadDivider3);
+            this.Controls.Add(this.RoadDivider2);
+            this.Controls.Add(this.RoadDivider1);
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadDivider8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EnemyCar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox RoadDivider1;
+        private System.Windows.Forms.Timer RoadAnimationTimer;
+        private System.Windows.Forms.PictureBox RoadDivider2;
+        private System.Windows.Forms.PictureBox RoadDivider3;
+        private System.Windows.Forms.PictureBox RoadDivider4;
+        private System.Windows.Forms.PictureBox RoadDivider5;
+        private System.Windows.Forms.PictureBox RoadDivider6;
+        private System.Windows.Forms.PictureBox RoadDivider7;
+        private System.Windows.Forms.PictureBox RoadDivider8;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox Car;
+        private System.Windows.Forms.Timer Left_mover;
+        private System.Windows.Forms.Timer Right_mover;
+        private System.Windows.Forms.PictureBox EnemyCar1;
+        private System.Windows.Forms.PictureBox EnemyCar2;
+        private System.Windows.Forms.PictureBox EnemyCar3;
+        private System.Windows.Forms.Timer Enemy1_timer;
+        private System.Windows.Forms.Timer Enemy2_timer;
+        private System.Windows.Forms.Timer Enemy3_timer;
+        private System.Windows.Forms.Label GameOverLabel;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Button ReplayButton;
+    }
+}
+
---
+++ RacingGameTestProject/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RacingGameTestProject
+{
+    public partial class Form1 : Form
+    {
+        int speed;
+        int score;
+        PictureBox[] road = new PictureBox[8];
+        int num;
+
+        public Form1()
+        {
+            InitializeComponent();
+            speed = 3;
+            road[0] = RoadDivider1;
+            road[1] = RoadDivider2;
+            road[2] = RoadDivider3;
+            road[3] = RoadDivider4;
+            road[4] = RoadDivider5;
+            road[5] = RoadDivider6;
+            road[6] = RoadDivider7;
+            road[7] = RoadDivider8;
+        }
+
+
+        private void RoadAnimationTimer_Tick(object sender, EventArgs e)
+        {
+
+            PictureBox display = new PictureBox();
+            display.Height = ClientRectangle.Height;
+            num = ClientRectangle.Height;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                road[i].Top += 2;
+                if (road[i].Top >= num)
+                {
+                    road[i].Top = -road[i].Height;
+                }
+                if (Car.Bounds.IntersectsWith(EnemyCar1.Bounds))
+                {
+                    GameOver();
+                }
+                if (Car.Bounds.IntersectsWith(EnemyCar2.Bounds))
+                {
+                    GameOver();
+                }
+                if (Car.Bounds.IntersectsWith(EnemyCar3.Bounds))
+                {
+                    GameOver();
+                }
+            }
+        }
+
+        private void GameOver()
+        {
+            GameOverLabel.Visible = true;
+            ReplayButton.Visible = true;
+            RoadAnimationTimer.Stop();
+            Enemy1_timer.Stop();
+            Enemy2_timer.Stop();
+            Enemy3_timer.Stop();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                Left_mover.Start();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                Right_mover.Start();
+            }
+        }
+
+        private void Left_mover_Tick(object sender, EventArgs e)
+        {
+            if (Car.Location.X > 0)
+            {
+                Car.Left -= 5;
+            }
+            
+        }
+
+        private void Right_mover_Tick(object sender, EventArgs e)
+        {
+            if (Car.Location.X < 310)
+            {
+                Car.Left += 5;
+            }
+            
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            Right_mover.Stop();
+            Left_mover.Stop();
+        }
+
+        private void Enemy1_timer_Tick(object sender, EventArgs e)
+        {
+            EnemyCar1.Top += 2;
+            if (EnemyCar1.Top >= num)
+            {
+                score += 1;
+                ScoreLabel.Text = "Score " + score;
+                Random random2 = new Random();
+                Double rdoub2 = random2.Next(0, 100);
+                EnemyCar1.Top = -EnemyCar1.Height;
+                EnemyCar1.Left = Convert.ToInt32(Math.Ceiling(rdoub2)) + 0;
+            }
+        }
+
+        private void Enemy2_timer_Tick(object sender, EventArgs e)
+        {
+            EnemyCar2.Top += 2;
+            if (EnemyCar2.Top >= num)
+            {
+                score += 1;
+                ScoreLabel.Text = "Score " + score;
+                Random random2 = new Random();
+                Double rdoub2 = random2.Next(0, 100);
+                EnemyCar2.Top = -EnemyCar2.Height;
+                EnemyCar2.Left = Convert.ToInt32(Math.Ceiling(rdoub2)) + 100;
+            }
+        }
+
+        private void Enemy3_timer_Tick(object sender, EventArgs e)
+        {
+            EnemyCar3.Top += 2;
+            if (EnemyCar3.Top >= num)
+            {
+                score += 1;
+                ScoreLabel.Text = "Score " + score;
+                Random random2 = new Random();
+                Double rdoub2 = random2.Next(0, 100);
+                EnemyCar3.Top = -EnemyCar3.Height;
+                EnemyCar3.Left = Convert.ToInt32(Math.Ceiling(rdoub2)) + 250;
+            }
+        }
+
+        private void ReplayButton_Click(object sender, EventArgs e)
+        {
+            score = 0;
+            GameOverLabel.Visible = false;
+            ReplayButton.Visible = false;
+            RoadAnimationTimer.Start();
+
+        }
+    }
+}
